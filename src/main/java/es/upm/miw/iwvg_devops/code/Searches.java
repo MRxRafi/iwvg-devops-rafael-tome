39ae11914199db2a0d9fac36ea72b4efaf13ff08
MRxRafi <rafadriveclase@gmail.com>
2020-09-20 20:39:57 +0200

    Search FindDecimalFractionByUserName and Test completed #10

---
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -0,0 +1,12 @@
+package es.upm.miw.iwvg_devops.code;
+
+import java.util.stream.Stream;
+
+public class Searches {
+    public Stream findDecimalFractionByUserName(String name){
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getName().equals(name))
+                .flatMap(user -> user.getFractions().stream()
+                        .map(Fraction::decimal));
+    }
+}
---
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -0,0 +1,17 @@
+package es.upm.miw.iwvg_devops.code;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SearchesTest {
+    @Test
+    void testFindDecimalFractionByUserName(){
+        assertEquals(List.of(0.0, 1.0, 2.0, 0.2, -0.5, 0.5, 1.0),
+                new Searches().findDecimalFractionByUserName("Oscar")
+                        .collect(Collectors.toList()));
+    }
+}
